5d315b8c47ffa6ba371b12a468e4b55b28afe97a
thiago0003 <thiago.martins@estudante.ufscar.br>
2023-10-27 20:12:43 -0300

    Remove folders

--- verilator_test/test_our/obj_dir/test_our/obj_dir/sim_main.d
+++
@@ -1,6 +0,0 @@
-sim_main.o: ../sim_main.cpp Vour.h \
- /home/esl/Documents/verilator/include/verilated.h \
- /home/esl/Documents/verilator/include/verilatedos.h \
- /home/esl/Documents/verilator/include/verilated_config.h \
- /home/esl/Documents/verilator/include/verilated_types.h \
- /home/esl/Documents/verilator/include/verilated_funcs.h
