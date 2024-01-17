ba8f2adfb5f3cbd1d1e082d1feeb5020a8e89b58
Thiago Martins <31443982+thiago0003@users.noreply.github.com>
2024-01-17 09:59:38 -0300

    Delete Frankenstein/obj_dir directory

--- Frankenstein/obj_dir/frankenstein.d
+++
@@ -1,9 +0,0 @@
-frankenstein.o: ../frankenstein.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_trace.h ../obj_dir/Vtop.h
